d5ff75a20aa6a70bd56f6d5134f7ce7e29fd5fe3
zsu0 <z.bell.sz12@gmail.com>
2025-05-19 10:48:05 +0700

    Create lexical.py

---
+++ lexical.py
@@ -0,0 +1,16 @@
+words_by_level = {
+    'A': [
+        "cat", "dog", "sun", "run", "big", "red", "man", "hat", "pen", "book",
+        "fish", "car", "bed", "cup", "fan", "box", "door", "egg", "fun", "game"
+    ],
+    'B': [
+        "electric", "flower", "machine", "jumping", "animal", "orange", "banana",
+        "chicken", "window", "bottle", "guitar", "planet", "travel", "pencil",
+        "number", "paper", "school", "summer", "winter", "mirror"
+    ],
+    'C': [
+        "jellybean", "toxic", "insightful", "philosophy", "algorithm", "encyclopedia",
+        "architecture", "revolutionary", "psychology", "transcendence", "phenomenon",
+        "environment", "metaphysical", "consciousness", "cryptography"
+    ]
+}
