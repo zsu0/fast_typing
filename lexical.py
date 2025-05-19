0a05b75ecc95de27f6337e8d030a2db1c36bc323
zsu0 <z.bell.sz12@gmail.com>
2025-05-19 11:11:32 +0700

    Update lexical.py

--- lexical.py
+++ lexical.py
@@ -1,16 +1,31 @@
+
 words_by_level = {
     'A': [
         "cat", "dog", "sun", "run", "big", "red", "man", "hat", "pen", "book",
-        "fish", "car", "bed", "cup", "fan", "box", "door", "egg", "fun", "game"
+        "fish", "car", "bed", "cup", "fan", "box", "door", "egg", "fun", "game",
+        "apple", "boy", "girl", "tree", "water", "milk", "ball", "leg", "hand", "foot",
+        "bag", "bus", "nose", "eye", "ear", "school", "rain", "wind", "sit", "stand",
+        "walk", "jump", "go", "eat", "drink", "yes", "no", "hot", "cold", "fast"
     ],
     'B': [
         "electric", "flower", "machine", "jumping", "animal", "orange", "banana",
         "chicken", "window", "bottle", "guitar", "planet", "travel", "pencil",
-        "number", "paper", "school", "summer", "winter", "mirror"
+        "number", "paper", "school", "summer", "winter", "mirror",
+        "library", "holiday", "airport", "doctor", "kitchen", "painter", "teacher",
+        "driver", "student", "camera", "message", "present", "market", "village",
+        "meeting", "clothes", "weather", "cousin", "family", "country",
+        "pillow", "computer", "battery", "subway", "address", "bicycle", "shower", "pocket",
+        "ticket", "backpack"
     ],
     'C': [
         "jellybean", "toxic", "insightful", "philosophy", "algorithm", "encyclopedia",
         "architecture", "revolutionary", "psychology", "transcendence", "phenomenon",
-        "environment", "metaphysical", "consciousness", "cryptography"
+        "environment", "metaphysical", "consciousness", "cryptography",
+        "optimization", "phenomenology", "hypothetical", "neurological", "quantitative",
+        "subconscious", "intelligence", "existential", "cognitive", "perception",
+        "aesthetic", "artificial", "probability", "transmission", "imagination",
+        "acceleration", "computation", "legislation", "anthropology", "bureaucracy",
+        "infrastructure", "sustainability", "reconciliation", "interpretation", "algorithmic",
+        "universal", "revolution", "predictive", "mathematical", "sociological", "economical", "jurisdiction"
     ]
 }
